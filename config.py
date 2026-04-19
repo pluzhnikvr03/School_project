75fc795cf2c46d5602a791e4faae087166d7c331
Victoria <150126357+pluzhnikvr03@users.noreply.github.com>
2026-04-19 21:10:13 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 token='your_token_is_here'  # токен Telegram бота
 ADMIN_ID = admin_id  # администратор
-BOT_EMAIL = "school_email@gmail.com"  # почта, с которой отправляется файл с qr
+# BOT_EMAIL = "school_email@gmail.com" - почта, с которой загружается на Google Диск файл с qr
