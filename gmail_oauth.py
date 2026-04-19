ea017796574dffc0ba940a8a424834331ffa510f
Victoria <150126357+pluzhnikvr03@users.noreply.github.com>
2026-04-19 22:26:15 +0300

    Update gmail_oauth.py

--- gmail_oauth.py
+++ gmail_oauth.py
@@ -4,7 +4,6 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 SCOPES = [
-    'https://www.googleapis.com/auth/gmail.send',
     'https://www.googleapis.com/auth/drive.file'  # разрешение на загрузку файлов на Диск
 ]
 
