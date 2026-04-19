e0bf158c2c1cfb51fc775f50c6373bfcc2d8d7d8
Victoria <150126357+pluzhnikvr03@users.noreply.github.com>
2026-04-19 21:21:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-token='your_token_is_here'  # токен Telegram бота
-ADMIN_ID = admin_id  # администратор
-# BOT_EMAIL = "school_email@gmail.com" - почта, с которой загружается на Google Диск файл с qr
+token='your_token_is_here'  # токен Telegram бота (необходимо получить у @BotFather)
+ADMIN_ID = admin_id  # Telegram ID администратора
+# BOT_EMAIL = "school_email@gmail.com" - почта, с которой загружается на Google Drive файл с QR
